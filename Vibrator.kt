35d13b2b38edb584b5f5b0f4537e95e05d4623d3
Nicolas Sauter <sauternic@users.noreply>
2019-03-24 02:11:35 +0100

    new file Vibrator.kt

---
+++ Vibrator.kt
@@ -0,0 +1,74 @@
+******************************MainActivity.kt***************************************************
+
+package com.example.myapplication
+
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button1 = findViewById<Button>(R.id.button)
+
+        button1.setOnClickListener {
+            val vibrator = getSystemService(VIBRATOR_SERVICE) as Vibrator
+
+            //deprecated:
+            //vibrator.vibrate(2000);
+
+            //New:
+            vibrator.vibrate(VibrationEffect.createOneShot(2000, VibrationEffect.DEFAULT_AMPLITUDE))
+        }
+    }
+
+}
+
+
+************************************************************************************************
+
+
+************************MainActivity.java*******************************************************
+
+package com.example.myapplication;
+
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = findViewById(R.id.button);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+
+                //deprecated:
+                //vibrator.vibrate(2000);
+
+                //New:
+                vibrator.vibrate(VibrationEffect.createOneShot(2000,VibrationEffect.DEFAULT_AMPLITUDE));
+            }
+        });
+    }
+	
+}
