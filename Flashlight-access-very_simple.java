ff7bbb0ffad65d8457b9dd3e4bcea1b4b0b957e6
Nicolas Sauter <sauternic@users.noreply>
2019-03-27 17:51:59 +0100

    while(true) entfernt

--- Flashlight-access-very_simple.java
+++ Flashlight-access-very_simple.java
@@ -12,21 +12,19 @@ import android.Manifest;
 import android.support.v4.app.ActivityCompat;
 
 	
-	while (true) {
+	
 		try {
 		    camera = Camera.open();
 		    Camera.Parameters parameters = camera.getParameters();
 		    parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
 		    camera.setParameters(parameters);
 		    camera.startPreview();
-		    break;
 		} catch (Exception e) {
 		    //request authorization-dialog to user
 		    ActivityCompat.requestPermissions(MainActivity.this,
 		               new String[]{Manifest.permission.CAMERA},
 		               1);
 		}
-	}
 	
 **************************************************************************
 
