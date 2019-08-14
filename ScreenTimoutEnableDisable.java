c3c24b6e2776274fd32912e93a17e1f88cc2f77d
Nicolas Sauter <sauternic@users.noreply>
2019-08-14 13:49:01 +0200

    ScreenTimoutEnableDisable.java

---
+++ ScreenTimoutEnableDisable.java
@@ -0,0 +1,9 @@
+
+
+// Screnn timoute disable
+getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+
+// Screen Timoute enable
+getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		
